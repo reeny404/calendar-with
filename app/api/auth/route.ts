4a20694cee56173a99c0f0ac596c53e43c08f010
reeny404 <happy2heyon@gmail.com>
2024-09-06 17:31:01 +0900

    update: 이미 저장된 accessToken이 있다면 insert 하지 않도록

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -3,25 +3,36 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
-    const { access_token, token_type, expires_in } = await request.json();
+    const { access_token, token_type, expires_in, error } = await request.json();
 
-    const supabase = createClient();
-    const { error } = await supabase
-      .from("token")
-      .insert({
-        social_type: 'google',
-        token_type,
-        access_token,
-        expire: expires_in
-      });
-    
     if (error) {
-      throw error;
+      throw new Error(error);
     }
 
-    return NextResponse.json({ type : token_type, accessToken : access_token });
+    const supabase = createClient();
+    const { data: list } = await supabase
+      .from("token")
+      .select()
+      .eq("access_token", access_token);
+
+    if (!list?.length) {
+      const { error: insertError } = await supabase
+        .from("token")
+        .insert({
+          social_type: 'google',
+          token_type,
+          access_token,
+          expire: expires_in
+        });
+
+      if (insertError) {
+        throw insertError;
+      }
+    }
+
+    return NextResponse.json({ type: token_type, accessToken: access_token });
   } catch (e) {
     console.error(e);
-    return NextResponse.json(e, { status : 500 });
+    return NextResponse.json(e, { status: 500 });
   }
 }
