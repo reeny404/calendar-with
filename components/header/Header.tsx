3cbb7d852e5d8dc07251f4591359900fa4d7691f
reeny404 <happy2heyon@gmail.com>
2024-10-04 21:42:49 +0900

    feat: 로그인 페이지 경로 뚫기

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 function Header() {
   return (
@@ -13,6 +14,7 @@ function Header() {
         />
       </div>
       <div className='flex space-x-2'>
+        <Link href='/login'>로그인</Link>
         <Image src='/icons/reload.svg' alt='sync' width={24} height={24} />
         <div className='w-6 h-6 flex items-center justify-center border-2 border-black rounded font-semibold'>
           {new Date().getDate()}
