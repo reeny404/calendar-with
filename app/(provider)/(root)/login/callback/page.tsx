585940cc96fdd35188beb65933dae17cccef0b58
reeny <happy2hyeon@kakao.com>
2025-03-21 18:00:19 +0900

    fix: ssr에서 window 객체에 접근하지못해 build error 오류나서 처리 - 로그인 콜백 기능을 AuthCallback.tsx로 분리하고, page.tsx에서 동적 임포트를 사용하도록 수정함. 이를 통해 코드 구조를 개선하고, SSR을 비활성화하여 클라이언트 측에서만 렌더링되도록 변경함.

--- app/(provider)/(root)/login/callback/page.tsx
+++ app/(provider)/(root)/login/callback/page.tsx
@@ -1,39 +1,9 @@
 'use client';
 
-import Loading from '@/components/Loading';
-import { BASE_URL } from '@/constants/env';
-import { ParamUtil } from '@/utils/ParamUtil';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { Cookies } from 'react-cookie';
+import dynamic from 'next/dynamic';
 
-function AuthCallbackPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const queryString: string = window.location.hash.replace('#', '');
-    const params = ParamUtil.parseString(queryString);
-    if (params.error) {
-      router.replace('/login/fail?error=' + JSON.stringify(params.error));
-      return;
-    }
-
-    fetch(BASE_URL + '/api/auth', {
-      method: 'POST',
-      body: JSON.stringify(params),
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        new Cookies().set('access_token', response.data.accessToken);
-        router.replace('/');
-      })
-      .catch((reason) => {
-        console.warn(reason);
-        router.replace('/login');
-      });
-  }, [router]);
-
-  return <Loading />;
-}
+const AuthCallbackPage = dynamic(() => import('./AuthCallback'), {
+  ssr: false
+})
 
 export default AuthCallbackPage;
