a59449b69f9d5a063c272e64338445f8c257974c
reeny <happy2hyeon@kakao.com>
2025-05-16 16:23:07 +0900

    feat: Princess Sofia 폰트를 추가하고 Header 컴포넌트의 링크 스타일 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,6 @@
 
 import { DateContext } from '@/context/DateContext';
 import { format } from 'date-fns';
-import Image from 'next/image';
 import Link from 'next/link';
 import { useContext, useMemo } from 'react';
 import { Button } from './ui/button';
@@ -18,8 +17,8 @@ export default function Header() {
 
   return (
     <header className='w-full h-header grid grid-cols-3 items-center justify-between'>
-      <Link href='/'>
-        <Image src='/logo.png' alt='logo' width={50} height={50} priority />
+      <Link href='/' className='font-princess-sofia text-2xl'>
+        Calendar With
       </Link>
       <h3 className='text-center'>{title}</h3>
       <div className='flex items-center justify-end gap-x-4'>
